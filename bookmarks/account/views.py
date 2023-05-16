2115f18d61145ac59f1f885313c8e1354fe252b6
Danil Mustafin <danil.mustafin921@gmail.com>
2023-05-17 00:51:02 +0500

    follow_01

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,13 +1,38 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 from .forms import (
     LoginForm, UserRegistrationForm,
     UserEditForm, ProfileEditForm)
 from .models import Profile
+
+
+@login_required
+def user_list(request):
+    template = 'account/user/list.html'
+    users = User.objects.filter(is_active=True)
+    context = {
+        'section': 'people',
+        'users': users
+        }
+    return render(request, template, context)
+
+
+@login_required
+def user_detail(request, username):
+    template = 'account/user/detail.html'
+    user = get_object_or_404(
+        User, username=username, is_active=True
+    )
+    context = {
+        'section': 'people',
+        'user': user
+    }
+    return render(request, template, context)
 
 
 def register(request):
--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -164,3 +165,7 @@ SOCIAL_AUTH_PIPELINE = [
 ]
 
 THUMBNAIL_DEBUG = True
+
+ABSOLUTE_URL_OVERRIDES = {
+    'auth.user': lambda u: reverse_lazy('account:user_detail', args=[u.username])
+}
